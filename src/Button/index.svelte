b533509763b43faa7aa46bb382bb598f9caa123f
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2020-09-04 10:25:21 +1000

    Attempt to replace CSS Flexbox gap
    Support for the gap for Flexbox is still insuficient.
    
    But the additional styles are not actually applied to compnents
    that fill the slot, so this is not a good solution.
    
    See https://caniuse.com/#feat=flexbox-gap

--- src/Button/index.svelte
+++ src/Button/index.svelte
@@ -9,12 +9,24 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        gap: var(--button-gap);
         min-height: var(--button-size);
         min-width: var(--button-size);
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+    }
+
+    /* Can be replaced by CSS gap when supported more widely. */
+    button > * {
+        margin: 0 calc(var(--button-gap) / 2);
+    }
+
+    button > *:first-child {
+        margin-left: 0;
+    }
+
+    button > *:last-child {
+        margin-right: 0;
     }
 
     .relaxed {
